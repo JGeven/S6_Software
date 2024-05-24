b5286735f2516eeb8fff04ca162a2547f23cac55
JGeven <j.geven@student.fontys.nl>
2024-05-24 15:37:50 +0200

    Updated maven requirements

--- User/src/main/java/mymanga/user/controller/UserController.java
+++ User/src/main/java/mymanga/user/controller/UserController.java
@@ -1,4 +1,26 @@
 package mymanga.user.controller;
 
+import mymanga.user.model.User;
+import mymanga.user.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequestMapping("user")
 public class UserController {
+
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<User> getUserByEmail(@RequestParam String email) {
+        User user = userService.getUserByEmail(email);
+        System.out.println(user);
+        return ResponseEntity.ok(user);
+    }
+
 }
--- User/src/test/java/mymanga/user/UserApplicationTests.java
+++ User/src/test/java/mymanga/user/UserApplicationTests.java
@@ -3,10 +3,10 @@ package mymanga.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class UserApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 

a7d15bd24ad1876cda1e4883d26e29431f9779a1
JGeven <j.geven@student.fontys.nl>
2024-06-20 23:31:05 +0200

    changed k8s files and added events for rabbitmq

--- Manga/src/main/java/mymanga/manga/controller/MangaController.java
+++ Manga/src/main/java/mymanga/manga/controller/MangaController.java
@@ -1,5 +1,6 @@
 package mymanga.manga.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import mymanga.manga.messagebroker.RabbitMQProducer;
 import mymanga.manga.model.Manga;
 import mymanga.manga.service.MangaService;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("manga")
 @RestController
@@ -43,6 +46,42 @@ public class MangaController {
         String url = "https://serverless-test.j-geven.workers.dev/";
         System.out.println(restTemplate.getForObject(url, String.class));
         return restTemplate.getForObject(url, String.class);
+    }
+
+    @PostMapping("/favorite")
+    public void favoriteManga() {
+        Map<String, Object> messageMap = new HashMap<>();
+        messageMap.put("mangaId", 1);
+        messageMap.put("mangaTitle", "Berserk");
+        messageMap.put("mangaChapter", 387);
+        messageMap.put("message", String.format("%s has just released a new chapter %d!", "Berserk", 387));
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonMessage = objectMapper.writeValueAsString(messageMap);
+
+            producer.sendMessage(jsonMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PostMapping("/liked")
+    public void likedManga() {
+        Map<String, Object> messageMap = new HashMap<>();
+        messageMap.put("reviewId", 1);
+        messageMap.put("userId", 15);
+        messageMap.put("mangaTitle", "Berserk");
+        messageMap.put("message", String.format("Your review for %s has received a new like!", "Berserk"));
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonMessage = objectMapper.writeValueAsString(messageMap);
+
+            producer.sendMessage(jsonMessage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- User/src/main/java/mymanga/user/controller/UserController.java
+++ User/src/main/java/mymanga/user/controller/UserController.java
@@ -1,14 +1,17 @@
 package mymanga.user.controller;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import mymanga.user.model.User;
 import mymanga.user.service.UserService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+
+import java.util.Map;
+
 
 @RequestMapping("user")
 @RestController
@@ -39,8 +42,31 @@ public class UserController {
     }
 
     @RabbitListener(queues = {"${rabbitmq.queue.name}"})
-    public void sendMessage(String mangaName) {
-        System.out.println("Consumed message, " + mangaName);
-    }
+    public void listen(String message) {
+        try {
+            // Parse the JSON message
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> messageMap = objectMapper.readValue(message, Map.class);
 
+            if (messageMap.containsKey("mangaId")) {
+                int mangaId = (int) messageMap.get("mangaId");
+                String mangaTitle = (String) messageMap.get("mangaTitle");
+                int mangaChapter = (int) messageMap.get("mangaChapter");
+
+                System.out.printf("Received message with mangaId %s: %s has just released a new chapter %d!%n", mangaId, mangaTitle, mangaChapter);
+            }
+
+            if (messageMap.containsKey("reviewId")) {
+                int reviewId = (int) messageMap.get("reviewId");
+                int userId = (int) messageMap.get("userId");
+                String mangaTitle = (String) messageMap.get("mangaTitle");
+
+                System.out.printf("Received message with reviewId %s and user id %d: Your review for %s has received a new like!", reviewId, userId, mangaTitle);
+            }
+
+        } catch (Exception e) {
+            // Handle the exception (e.g., logging)
+            e.printStackTrace();
+        }
+    }
 }
